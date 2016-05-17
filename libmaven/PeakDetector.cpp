5f7a7ebef37e6d3d186938932e162d038194ab74
Naman <namanspace@gmail.com>
2016-05-17 18:10:13 +0530

    new class in libmaven for handling peak detection

---
+++ libmaven/PeakDetector.cpp
@@ -0,0 +1,18 @@
+/*
+ * PeakDetector.cpp
+ *
+ *  Created on: May 17, 2016
+ *      Author: naman
+ */
+
+#include "PeakDetector.h"
+
+PeakDetector::PeakDetector() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PeakDetector::~PeakDetector() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ libmaven/PeakDetector.h
@@ -0,0 +1,17 @@
+/*
+ * PeakDetector.h
+ *
+ *  Created on: May 17, 2016
+ *      Author: naman
+ */
+
+#ifndef LIBMAVEN_PEAKDETECTOR_H_
+#define LIBMAVEN_PEAKDETECTOR_H_
+
+class PeakDetector {
+public:
+	PeakDetector();
+	virtual ~PeakDetector();
+};
+
+#endif /* LIBMAVEN_PEAKDETECTOR_H_ */
