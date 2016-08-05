a9b389a9e499c8b68d77ea152efb6e9c510fa66a
Sabu_George <sabu.george@elucidata.io>
2016-08-06 00:42:41 +0530

    Testing implemnted in a different way .pro file updatd in build.pro 1) Test case now has a main function that is explicetely defined 2) .pro for the test isshifted to build.pro 3) test.sh is not needed as the compilation of and running of test case is taken care inside the run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 OMP_CANCELLATION=true
 qmake -qt=qt4 -o Makefile build.pro
 make -j $(nproc)
+
+qmake -qt=qt4 -o MavenTests/Makefile build.pro CONFIG+=test
+make -C MavenTests/ all $(nproc)
+
+if [ -f MavenTests/test.xml ]; then
+	rm MavenTests/test.xml
+fi
+
+if [ -f MavenTests/MavenTests ]; then
+	./MavenTests/MavenTests -xml -o MavenTests/test.xml
+fi
+
