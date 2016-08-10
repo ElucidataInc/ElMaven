fd5f5339dd2621790bf5a65c071a7183536714e8
sahil21794 <sahil@elucidata.io>
2016-08-10 18:06:20 +0530

    Moved some variables from paths to constants & also changed there names to input_paths & arguments respectively

--- config/config.py
+++ config/config.py
@@ -1,39 +1,37 @@
 
-class paths():
+class input_paths():
 
 	def __init__(self, build_maven):
 
 		#For build 769
 		if build_maven == 769:
 			self.path_peakdetector = '/home/sahil/elucidata/maven/mavenOpenSource/bin/peakdetector'
-			self.outputdir = '/home/sahil/Desktop/769'
 
 		#For build 776
 		if build_maven == 776:
 			self.path_peakdetector = '/home/sahil/code/qe/maven_stable/bin/peakdetector'
-			self.outputdir = '/home/sahil/Desktop/776'
 
 		#For all builds
 		self.inputdir = '/home/sahil/Desktop/Dataset'
-		self.db = '/home/sahil/code/qe/maven_data/CDatabases/qe3_v11_2016_04_13.tab.txt'
-		self.model = '/home/sahil/code/qe/maven_data/Models/default.model'
 
 
-class constants():
+class arguments():
 
 	def __init__(self, build_maven):
 
 		#For build 769
 		if build_maven == 769:
-			pass
+			self.outputdir = '/home/sahil/Desktop/769'
 		
 		#For build 776
 		if build_maven == 776:
-			
+			self.outputdir = '/home/sahil/Desktop/776'
 			self.minPrecursorCharge = 0
 			self.pullIsotopes = 0
-		
+
 		#For all builds
+		self.db = '/home/sahil/code/qe/maven_data/CDatabases/qe3_v11_2016_04_13.tab.txt'
+		self.model = '/home/sahil/code/qe/maven_data/Models/default.model'
 		self.minGoodGroupCount = 1
 		self.eicMaxGroups = 5	
 		self.savemzroll = 1
@@ -65,4 +63,5 @@ class variables():
 		self.path_r2_values_csv = '/home/sahil/Desktop/comparison/r2_values.csv'
 		self.path_csv_with_diff_rt_mz = '/home/sahil/Desktop/comparison/diff_rt_mz_values.csv'
 		self.path_summary_of_results = '/home/sahil/Desktop/comparison/summary_of_results.docx'
+
 variables = variables()
