979e2ae9243aaf29016769c6f1cee0600cde2a01
sahil21794 <sahil@elucidata.io>
2016-08-11 23:55:06 +0530

    Added members in class variables in the config file

--- config/config.py
+++ config/config.py
@@ -1,3 +1,5 @@
+import time
+import datetime
 
 class input_paths():
 
@@ -22,7 +24,7 @@ class arguments():
 		#For build 769
 		if build_maven == 769:
 			self.outputdir = '/home/sahil/Desktop/769'
-		
+
 		#For build 776
 		if build_maven == 776:
 			self.outputdir = '/home/sahil/Desktop/776'
@@ -33,13 +35,13 @@ class arguments():
 		self.db = '/home/sahil/code/qe/maven_data/CDatabases/qe3_v11_2016_04_13.tab.txt'
 		self.model = '/home/sahil/code/qe/maven_data/Models/default.model'
 		self.eicMaxGroups = 5	
-		self.savemzroll = 1
+		self.savemzroll = [0,1]
 		self.minQuality = 0.5
 		self.ppmMerge = 10
 		self.minSignalBaseLineRatio = 2
 		self.grouping_maxRtWindow = 0.5
 		self.alignSamples = 0
-		self.minGroupIntensity = 10000
+		self.minGroupIntensity = [10000, 5000]
 		self.matchRtFlag = 0
 		self.minPeakWidth = 5
 		self.minGoodGroupCount = 3
@@ -58,9 +60,15 @@ class variables():
 		#	4 for processMassSlices with pullIsotopes (includes Reporting isotopic Peaks)
 		self.run_function = 1
 
-		self.path_compare_compounds_csv = '/home/sahil/Desktop/comparison/compare_compounds.csv'
-		self.path_r2_values_csv = '/home/sahil/Desktop/comparison/r2_values.csv'
-		self.path_csv_with_diff_rt_mz = '/home/sahil/Desktop/comparison/diff_rt_mz_values.csv'
-		self.path_summary_of_results = '/home/sahil/Desktop/comparison/summary_of_results.docx'
+		ts = time.time()
+		self.st = \
+		datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+
+		self.csv_compare_output = '/home/sahil/Desktop/comparison/'
+		self.csv_compare_compounds = 'compare_compounds.csv'
+		self.csv_r2_values = 'r2_values.csv'
+		self.csv_with_diff_rt_mz = 'diff_rt_mz_values.csv'
+		self.csv_summary_of_results = 'summary_of_results.docx'
+		self.plots_comparison = '/home/sahil/Desktop/plots'
 
 variables = variables()
