5e3247673ea9c7ca6c0e4093e27e63bc817ca786
sahil21794 <sahil@elucidata.io>
2016-08-11 23:53:05 +0530

    Imported plots and called function plot to generate comparison graphs

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-
+import plots
 import run_peakdetector
 import compare_output
 
 list_of_builds = [769, 776]
 run_peakdetector.run_peakdetector.run(list_of_builds)
 compare_output.compare_output.compare(list_of_builds)
+plots.plots.plot_comparison(list_of_builds)
