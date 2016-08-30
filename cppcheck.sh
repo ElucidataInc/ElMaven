ed053b22a9abd14c6c7344ffae8a218f9bc3a6d0
Sabu_George <sabu.george@elucidata.io>
2016-08-30 08:45:46 +0530

    warning: added lib in cppcheck to not show warning

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,2 +1,2 @@
 # cppcheck -j $(nproc) --enable=all --suppress='*:libcdfread/*' --suppress='*:libneural/*' --suppress='*:Eigen/*' --suppress='*:libcsvparser/*' --suppress='*:pugixml/*' --suppress='*:mzWatcher/*' --inconclusive --xml --xml-version=2 * 2> cppcheck.xml
-cppcheck -j $(nproc) --enable=all -itest/ -ilibcdfread/ -ilibneural/ -iEigen/ -ilibcsvparser/ -ipugixml/ -imzWatcher/ --inconclusive --xml --xml-version=2 * 2> cppcheck.xml
+cppcheck -j $(nproc) --enable=all -itest/ -ilibcdfread/ -ilibneural/ -iEigen/ -ilibcsvparser/ -ipugixml/ -imzWatcher/ -ilibpls/ --inconclusive --xml --xml-version=2 * 2> cppcheck.xml
