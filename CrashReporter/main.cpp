187e446645a3cc1714344a8ab1e20b673decdda4
Sabu George <sabu.george@elucidata.io>
2017-02-04 19:17:12 +0530

    fix: restart maven from crash reporter

--- CrashReporter/main.cpp
+++ CrashReporter/main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    w.restartApplicationPath = QString(argv[1]);
+
     w.show();
 
     return a.exec();
--- CrashReporter/mainwindow.cpp
+++ CrashReporter/mainwindow.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->restartApplicationPath = "";
+    connect(restart, SIGNAL(clicked()), this, SLOT(on_restart_clicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_restart_clicked()
+{
+    this->startElMaven();
+}
+
+void MainWindow::startElMaven()
+{
+    QProcess * myProcess = new QProcess();
+    myProcess->start(this->restartApplicationPath);
+    QCoreApplication::quit();
 }
