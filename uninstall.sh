4ff37945ad7aa9630c3dc3c2794dcb12f41d462e
Sahil <sahil@elucidata.io>
2017-02-20 16:41:15 +0530

    Ref: Remove .a libs from build/lib directory when uninstalling whole project

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 make distclean
+rm -rf build/lib/*
 make -C MavenTests/ distclean
 rm -rf bin/maven_dev* bin/El_Maven* bin/peakdetector bin/mzWatcher docs/html docs/latex 
 rm -rf $HOME/.config/mzRoll
