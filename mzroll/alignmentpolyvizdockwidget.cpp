e27baec65b38d129500917566a80662faed0b891
Sahil <sahil@elucidata.io>
2017-03-01 16:09:46 +0530

    Ftr: Add toolBar & set window title for alignmenpolydockwidget

--- mzroll/alignmentpolyvizdockwidget.cpp
+++ mzroll/alignmentpolyvizdockwidget.cpp
@@ -5,6 +5,23 @@ AlignmentPolyVizDockWidget::AlignmentPolyVizDockWidget(QWidget *parent) :
     ui(new Ui::AlignmentPolyVizDockWidget)
 {
     ui->setupUi(this);
+    setObjectName("AlignmentPolyVizWidget");
+    setWindowTitle("AlignmentPolyVizWidget");
+
+    QToolBar *toolBar = new QToolBar(this);
+    toolBar->setFloatable(false);
+    toolBar->setMovable(false);
+
+    QWidget* spacer = new QWidget();
+    spacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    toolBar->addWidget(spacer);
+
+    QToolButton *btnHide = new QToolButton(toolBar);
+    btnHide->setIcon(style()->standardIcon(QStyle::SP_DialogCloseButton));
+    connect(btnHide, SIGNAL(clicked()),SLOT(hide()));
+    toolBar->addWidget(btnHide);
+
+    setTitleBarWidget(toolBar);
 }
 
 AlignmentPolyVizDockWidget::~AlignmentPolyVizDockWidget()
