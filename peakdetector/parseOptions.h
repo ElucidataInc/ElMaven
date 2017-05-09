5d1d121cbc2c055b465ca505135be775601e320f
Sahil <sahil@elucidata.io>
2017-05-09 19:23:34 +0530

    Ftr: Add basic functionality to load and parse XML file for CLI
    - loadXMLNode: load single parent node of a given file
    - getChildren: get all the children of a node
    - getChild : get particular child of a node
    - getAttributes: get map (name, value) of all the attributes
      present inside a node

--- peakdetector/parseOptions.h
+++ peakdetector/parseOptions.h
@@ -17,6 +17,36 @@ class ParseOptions {
         ParseOptions(struct Arguments &arguments);
         ~ParseOptions();
         void createXMLFile(struct Arguments &arguments);
+
+		/**
+		* [load single parent node of a given file]
+		* @param filename [xml file to be loaded]
+		* @param nodeName [name of the parent node to be loaded]
+        * @return node [parent node]
+		*/
+        xml_node loadXMLNode(char* filename, char* nodeName);
+
+		/**
+		* [get all the children of a node]
+		* @param node [parent node]
+        * @return nodeChildren [vector of children nodes of parent node]
+		*/
+        vector<xml_node> getChildren(xml_node &node);
+
+		/**
+		* [get particular child of a node]
+		* @param node [parent node]
+		* @param childName [name of child]
+        * @return nodeChild [child node]
+		*/
+        xml_node getChild(xml_node &node, char* childName);
+
+		/**
+		* [get map (name, value) of all the attributes present inside a node]
+		* @param node [parent node]
+		* @return attributes [QMap<string, string> of attributes from parent node]
+		*/
+        QMap<string, string> getAttributes(xml_node &node);
         void addChildren(xml_node args, char* nodeName, QStringList cliArguments);
         char* qStringtocharPointer(QString stringToBeConverted);
 
