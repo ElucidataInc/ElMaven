a3ff5189adaee1ad727812880c6bda4c0369b7bc
r-el-maya <raghuram.reddy@elucidata.io>
2017-06-27 20:14:53 +0530

    Fix: Added rt to the criteria. Will have to add rt_tolerance.

--- mzroll/mainwindow.cpp
+++ mzroll/mainwindow.cpp
@@ -2693,7 +2693,7 @@ void MainWindow::UndoAlignment() {
 vector<mzSlice*> MainWindow::getSrmSlices(double amuQ1, double amuQ3, bool associateCompoundNames) {
 	//Merged with Maven776 - Kiran
     qDebug() << "getSrmSlices() Q1=" << amuQ1 << " Q3=" << amuQ3;
-    QSet<QString>seenMRMS;
+    QMap<QString, Scan*>seenMRMS;
     //+118.001@cid34.00 [57.500-58.500]
     //+ c ESI SRM ms2 102.000@cid19.00 [57.500-58.500]
     //-87.000 [42.500-43.500]
@@ -2706,62 +2706,73 @@ vector<mzSlice*> MainWindow::getSrmSlices(double amuQ1, double amuQ3, bool assoc
 
     vector<mzSlice*>slices;
     for(int i=0; i < samples.size(); i++ ) {
-    	mzSample* sample = samples[i];
+        mzSample* sample = samples[i];
         for( int j=0; j < sample->scans.size(); j++ ) {
             Scan* scan = sample->getScan(j);
             if (!scan) continue;
 
-            QString filterLine(scan->filterLine.c_str());            
+            QString filterLine(scan->filterLine.c_str());
 
-            if (filterLine.isEmpty() or seenMRMS.contains(filterLine)) continue;
-            seenMRMS.insert(filterLine);
+            if (filterLine.isEmpty())   continue;
 
+            if (seenMRMS.contains(filterLine)){
+                if(scan->intensity[0] <= seenMRMS.value(filterLine)->intensity[0])    continue;
+            }
 
-            mzSlice* s = new mzSlice(0,0,0,0);
-            s->srmId = scan->filterLine.c_str();
-            slices.push_back(s);
+            seenMRMS.insert(filterLine, scan);
+        }
+    }
 
-            if (associateCompoundNames) {
-                //match compounds
-                Compound* compound = NULL;
-                float precursorMz = scan->precursorMz;
-                float productMz   = scan->productMz;
-                int   polarity= scan->getPolarity();
-                if (polarity==0) filterLine[0] == '+' ? polarity=1 : polarity =-1;
-                if (getIonizationMode()) polarity=getIonizationMode();  //user specified ionization mode
+    QMapIterator<QString, Scan*> iter(seenMRMS); 
 
-                if ( precursorMz == 0 ) {
-                    if( rx1a.indexIn(filterLine) != -1 ) {
-                        precursorMz = rx1a.capturedTexts()[1].toDouble();
-                    } else if ( rx1b.indexIn(filterLine) != -1 ) {
-                        precursorMz = rx1b.capturedTexts()[1].toDouble();
-                    }
-                }
+	int i=0;
+    for (auto filterLine: seenMRMS.keys()){
+		
+        Scan* scan = seenMRMS.value(filterLine);
+        mzSlice* s = new mzSlice(0,0,0,0);
+		s->srmId = scan->filterLine.c_str();
+        slices.push_back(s);
 
-                if (productMz == 0) {
-                    if ( rx2.indexIn(filterLine) != -1 ) {
-                        float lb = rx2.capturedTexts()[1].toDouble();
-                        float ub = rx2.capturedTexts()[2].toDouble();
-                        productMz = lb+(ub-lb)/2;
-                    }
-                }
+        if (associateCompoundNames) {
+            //match compounds
+            Compound* compound = NULL;
+            float precursorMz = scan->precursorMz;
+            float productMz   = scan->productMz;
+            float rt = scan->rt;
+            int   polarity= scan->getPolarity();
+            if (polarity==0) filterLine[0] == '+' ? polarity=1 : polarity =-1;
+            if (getIonizationMode()) polarity=getIonizationMode();  //user specified ionization mode
 
-                if (precursorMz != 0 && productMz != 0 ) {
-                    compound = DB.findSpeciesByPrecursor(precursorMz,productMz,polarity,amuQ1,amuQ3);
-                }
-
-                if (compound) {
-                    compound->srmId = filterLine.toStdString();
-                    s->compound = compound;
-                    s->rt = compound->expectedRt;
-                    countMatches++;
+            if ( precursorMz == 0 ) {
+                if( rx1a.indexIn(filterLine) != -1 ) {
+                    precursorMz = rx1a.capturedTexts()[1].toDouble();
+                } else if ( rx1b.indexIn(filterLine) != -1 ) {
+                    precursorMz = rx1b.capturedTexts()[1].toDouble();
                 }
             }
-        }
-        qDebug() << "SRM mapping: " << countMatches << " compounds mapped out of " << seenMRMS.size();
-    }
-    return slices;
-}
+
+            if (productMz == 0) {
+                if ( rx2.indexIn(filterLine) != -1 ) {
+                    float lb = rx2.capturedTexts()[1].toDouble();
+                    float ub = rx2.capturedTexts()[2].toDouble();
+                    productMz = lb+(ub-lb)/2;
+                }
+            }
+
+            if (precursorMz != 0 && productMz != 0 ) {
+                compound = DB.findSpeciesByPrecursor(precursorMz,productMz,rt,polarity,amuQ1,amuQ3);
+            }
+
+            if (compound) {
+                compound->srmId = filterLine.toStdString();
+                s->compound = compound;
+                s->rt = compound->expectedRt;
+                countMatches++;
+            }
+        }                       
+	}
+	return slices;
+}                                    
 
 void MainWindow::showPeakInfo(Peak* _peak) {
 	if (_peak == NULL)
