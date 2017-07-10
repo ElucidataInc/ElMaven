f16827cdd99c8bcbf0d2e8d407bcf68bf1b2a376
r-el-maya <raghuram.reddy@elucidata.io>
2017-07-10 20:16:01 +0530

    Ref: Create new file SRMList.h in libmaven #260
    To separate the algo part from the ui, created a new file SRMList.h
    that has functions with algo related to SRMList.
    
    Issue: 260

---
+++ libmaven/SRMList.h
@@ -0,0 +1,22 @@
+#ifndef SRMLIST_H
+#define SRMLIST_H
+
+#include "Compound.h"
+#include "databases.h"
+#include "mzSample.h"
+#include "Scan.h"
+ #include <QMap> 
+
+class SRMList{
+    public:
+
+        SRMLists(vector<mzSample*>samples, deque<Compound*> compoundsDB);
+        vector<mzSample*>samples;
+
+        vector<mzSlice*> getSrmSlices(double amuQ1, double amuQ3, int userPolarity, bool associateCompoundNames);
+        deque<Compound*> compoundsDB;
+
+        Compound* findSpeciesByPrecursor(float precursorMz, float productMz, float rt, int polarity,double amuQ1, double amuQ3);
+};
+
+#endif 
