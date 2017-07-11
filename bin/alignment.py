082fd7490daa71f34b2137834af0e652e7175c72
Raghav Sehgal <raghav.sehgal@elucidata.io>
2017-07-11 09:18:55 +0530

    Adding hard-coded parameters to alignment
    This part of the code reads the csv files outputted by the C++.
    It also fixes certain parameters and variables which would be
    required by the algorithm

--- bin/alignment.py
+++ bin/alignment.py
@@ -2,3 +2,13 @@ import pandas as pd
 import numpy as np
 from scipy.interpolate import interp1d
 import statsmodels.api as sm
+
+groups = pd.read_csv('groups.csv')
+rts = pd.read_csv('rts.csv')
+minFraction = 0.9
+extraPeaks = 1
+span = 0.2
+samples = rts.sample_name.unique()
+nSamples = len(samples)
+group_num = groups.group_number.unique()
+minSample = nSamples * minFraction
