928da47056eefe58d8eba77d62cb97efdccafc10
Raghav Sehgal <raghav.sehgal@elucidata.io>
2017-07-11 10:55:10 +0530

    Modifying code to eliminate groups better
    Previously groups were compared to total number of peaks in the group
    now they are being compared to unique samples. This improves the algo
    by avoiding missing cases.

--- bin/alignment.py
+++ bin/alignment.py
@@ -16,10 +16,11 @@ minSample = nSamples * minFraction
 lim_groups = pd.DataFrame()
 for i in group_num:
 	sub_groups = groups[groups.group_number == i]
-	rows,cols = sub_groups.shape
-	if rows >= minSample:
+	sub_grp_samp = groups.sample_name.unique()
+	nsamp = len(sub_grp_samp)
+	if nsamp >= minSample:
 		flag = False
-		for j in samples:
+		for j in sub_grp_samp:
 			sample_groups = sub_groups[sub_groups.sample_name == j]
 			row,col = sample_groups.shape
 			if row > extraPeaks + 1 :
