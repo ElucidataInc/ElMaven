6946e91d844d5e67963d33cc7f5b35f53b5eeda6
Pankaj Kumar <pankaj.kumar@elucidata.io>
2017-08-19 02:39:12 +0530

    testing travis

--- MavenTests/main.cpp
+++ MavenTests/main.cpp
@@ -84,7 +84,6 @@ int readLog(QString filename){
 
     QFile file(filename);
     int result=0;
-    
     if(file.open(QFile::ReadOnly | QFile::Text)){
         QXmlStreamReader xsr(&file);
         xsr.readNext();
