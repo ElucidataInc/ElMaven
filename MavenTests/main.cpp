04d6809ffa3b410919b56f266f4bfc228c8f25ee
Pankaj Kumar <pankaj.kumar@elucidata.io>
2017-08-18 12:31:45 +0530

    testing: checking travis config and rebasing
    this commit has rebasing of ftr_commandlines_for_failing_tests
    over ftr_testcases_travisCI over fix_failingTests and also has
    change to fail travis if a testcase is failing.
    
    issue #325

--- MavenTests/main.cpp
+++ MavenTests/main.cpp
@@ -14,7 +14,7 @@
 #include "testCLI.h"
 #include "testCharge.h"
 
-void readLog(QString);
+int readLog(QString);
 
 int main(int argc, char** argv) {
     QCoreApplication app(argc, argv);
@@ -26,63 +26,64 @@ int main(int argc, char** argv) {
 
     if (freopen("testLoadSamples.xml", "w", stdout))
         result |= QTest::qExec(new TestLoadSamples, argc, argv);
-    readLog("testLoadSamples.xml");
+    result|=readLog("testLoadSamples.xml");
 
     if (freopen("testMassCalculator.xml", "w", stdout))
         result |= QTest::qExec(new TestMassCalculator, argc, argv);
-    readLog("testMassCalculator.xml");
+    result|=readLog("testMassCalculator.xml");
     
     if(freopen("testCLI.xml",  "w", stdout))
         result |= QTest::qExec(new TestCLI, argc, argv);
-    readLog("testCLI.xml");
+    result|=readLog("testCLI.xml");
 
     if (freopen("testPeakDetection.xml", "w", stdout))
         result |= QTest::qExec(new TestPeakDetection, argc, argv);
-    readLog("testPeakDetection.xml");
+    result|=readLog("testPeakDetection.xml");
 
     if(freopen("testCharge.xml",  "w", stdout))
         result |= QTest::qExec(new TestCharge, argc, argv);
-    readLog("testCharge.xml");
+    result|=readLog("testCharge.xml");
 
     if (freopen("testLoadDB.xml", "w", stdout))
         result |= QTest::qExec(new TestLoadDB, argc, argv);
-    readLog("testLoadDB.xml");
+    result|=readLog("testLoadDB.xml");
 
     if (freopen("testMzSlice.xml", "w", stdout))
         result |= QTest::qExec(new TestMzSlice, argc, argv);
-    readLog("testMzSlice.xml");
+    result|=readLog("testMzSlice.xml");
 
     if (freopen("testScan.xml", "w", stdout))
         result |= QTest::qExec(new TestScan, argc, argv);
-    readLog("testScan.xml");
+    result|=readLog("testScan.xml");
 
     if(freopen("testEIC.xml",  "w", stdout))
         result |= QTest::qExec(new TestEIC, argc, argv);
-    readLog("testEIC.xml");
+    result|=readLog("testEIC.xml");
 
     if(freopen("testbase64.xml",  "w", stdout))
         result |= QTest::qExec(new Testbase64, argc, argv);
-    readLog("testbase64.xml");
+    result|=readLog("testbase64.xml");
 
     if(freopen("testMzFit.xml",  "w", stdout))
         result |= QTest::qExec(new TestMzFit, argc, argv);
-    readLog("testMzFit.xml");
+    result|=readLog("testMzFit.xml");
 
     if (freopen("testCSVReports.xml", "w", stdout))
         result |= QTest::qExec(new TestCSVReports, argc, argv);
-    readLog("testCSVReports.xml");
+    result|=readLog("testCSVReports.xml");
     
     // if(freopen("testMzAligner.xml",  "w", stdout))
     //     result |= QTest::qExec(new TestMzAligner, argc, argv);
-    // readLog("testMzAligner.xml");
+    // result|=readLog("testMzAligner.xml");
 
     return result;
 }
 
 
-void readLog(QString filename){
+int readLog(QString filename){
 
     QFile file(filename);
+    int result=0;
     if(file.open(QFile::ReadOnly | QFile::Text)){
         QXmlStreamReader xsr(&file);
         xsr.readNext();
@@ -97,8 +98,10 @@ void readLog(QString filename){
                 qDebug()<<"test case failed: file-"<<file<<", line-"<<line<<", error info: "<<xsr.readElementText();
             }
             xsr.readNext();
+            result=1;
         }
 
         file.close();
     }
+    return result;
 }
