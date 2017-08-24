83029a4c06f05bd15c1ba910538224e4dda687cd
Pankaj Kumar <pankaj.kumar@elucidata.io>
2017-08-24 19:32:22 +0530

    Fix: Add progress bar for json export #347
    All the changes in libmaven folder has been done in standard c++ but
    while implementing Progress Bar for json export, there was no way to
    take it to libmaven because Progress Bar is part of Qt GUI.
    
    Now Progress Bar has been set as waiting bar by giving value 0 for range
    and 100 for setValue.
    
    Issue: #347

--- mzroll/saveJson.cpp
+++ mzroll/saveJson.cpp
@@ -1,6 +1,9 @@
 #include "saveJson.h"
 void saveJson::run()
 {
+	Q_EMIT(_pt->updateProgressBar("Writing to json file. Please wait...", 100, 0));
     jsonReports=new JSONReports(_mw->mavenParameters);
     jsonReports->saveMzEICJson(_fn,_pt->vallgroups,_mw->getVisibleSamples());
+    Q_EMIT(_pt->updateProgressBar("Writing to json complete.", 100, 100));
+
 }
