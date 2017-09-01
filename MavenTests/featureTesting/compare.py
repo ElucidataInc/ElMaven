b4c09bf5eefc17552b147057f5c411d5b8ec99b2
Sahil <sahil@elucidata.io>
2017-09-01 15:55:01 +0530

    Test: Add compare module to compare different outputs #339
    Different instances of CLI will generate different outputs from
    different configuration files. Every output should be compared
    with output from previous version (having same configuration file)
    
    Add Class CompareOutput which will compare CSV output
    
    Issue: #339

---
+++ MavenTests/featureTesting/compare.py
@@ -0,0 +1,26 @@
+"""
+This module compares different output of cli
+
+List of functions:
+
+1. compare: Compare all the outputs generated from multiple
+    cli instances
+
+"""
+
+
+class CompareOutput(object):
+    """
+    This class compares different outputs of Cli generaed using
+    different configuration files
+    """
+
+    def __init__(self):
+        pass
+
+    def compare(self):
+        """
+        Compare all the outputs generated from multiple
+        cli instances
+        """
+        pass
