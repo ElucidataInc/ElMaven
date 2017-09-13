9474f20815d743fbed43202e50fbaf8594990c8b
Sahil <sahil@elucidata.io>
2017-09-13 20:37:48 +0530

    Ftr: Remove outliers from the merged pandas df #339
    Outliers are those groups (or rows) which do not have similar
    mzs and rts. Having unequal mzs and rts means both rows (not merged
    into one) are not the same group
    
    Add function remove_outliers in compare class to take of outliers
    on the basis of rt and mz values
    
    Issue: #339

--- MavenTests/featureTesting/compare.py
+++ MavenTests/featureTesting/compare.py
@@ -5,8 +5,11 @@ List of functions:
 
 1. compare: Compare all the outputs generated from multiple
     cli instances
+
 2. load_files: Loads every file present in the input list into a
     pandas dataframe
+
+3. remove_outliers: Remove rows whose mzs and rts are not equal
 
 """
 
@@ -31,6 +34,7 @@ class CompareOutput(object):
         df_list = self.load_files(file_list)
         col_list = ["compoundId", "compound", "formula", "goodPeakCount"]
         merged_df = helper.merge_dfs(df_list, col_list)
+        merged_df = self.remove_outliers(merged_df)
 
     def load_files(self, file_list):
         """
@@ -48,3 +52,30 @@ class CompareOutput(object):
             df_list.append(pdataframe)
 
         return df_list
+
+    def remove_outliers(self, pandas_df):
+        """
+        Remove rows whose mzs and rts are not equal
+        Args:
+            pandas_df (df): Merged pandas dataframe
+        Returns:
+            pandas_df (df): Pandas dataframe after outliers
+                are removed
+        """
+
+        for index, row in pandas_df.iterrows():
+
+            mz_1 = row["medMz_x"]
+            mz_2 = row["medMz_y"]
+            rt_1 = row["medRt_x"]
+            rt_2 = row["medRt_y"]
+
+            mz_diff = abs(mz_2-mz_1)
+            rt_diff = abs(rt_2-rt_1)
+
+            if mz_diff < 0.5 and rt_diff < 0.3:
+                pass
+            else:
+                pandas_df.drop(index, inplace=True)
+
+        return pandas_df
