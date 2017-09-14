bbed4d28e56732b5b25d30b6e06e15add0272baa
Sahil <sahil@elucidata.io>
2017-09-14 11:33:39 +0530

    Ftr: Add function to execute comparison of single config #339
    Multiple configuration files (xml files) will be available for
    comparsion. At one point only one configuration file will be
    executed and compared.
    
    Add function compare_output in main.py to do end-end workflow
    from running the cli instance to comparing the output files
    
    Issue: #339

--- MavenTests/featureTesting/main.py
+++ MavenTests/featureTesting/main.py
@@ -1,6 +1,7 @@
 """
 This runs the feature testing
 """
+import os
 import run_cli
 import compare
 from config import config
@@ -12,4 +13,25 @@ def main():
     """
     conf = config.Config()
 
-    xml_files = helper.list_files(conf.cli_configs_path, ".xml")
+    config_files = helper.list_files(conf.cli_configs_path, ".xml")
+
+    for config_file in config_files:
+        compare_output(config_file)
+
+def compare_output(config_path):
+    """
+    Compare output of one configration file
+    Args:
+        config_path (str): Path of configuration file
+    """
+    conf = config.Config()
+    output_dir = conf.output_path
+
+    helper.delete_dir(output_dir)
+    helper.make_dir(output_dir)
+    cli = run_cli.RunCli()
+    cli.run_cli(config_path)
+
+
+
+main()
