b9bcbeb1fc8c3dee8edf382dfdf1fcd51356c261
Sahil <sahil@elucidata.io>
2017-09-14 10:30:57 +0530

    Ftr: Get config files in main.py #339
    Configuration files (xml files) will be used to run the peak detector
    cli with different parameters (configurations) to generate various
    outputs to be compared.
    
    Use function list_files to get all the xml configs in main.py
    
    Issue: #339

--- MavenTests/featureTesting/main.py
+++ MavenTests/featureTesting/main.py
@@ -1,10 +1,15 @@
 """
 This runs the feature testing
 """
-
+import run_cli
+import compare
+from config import config
 from helper import helper
 
 def main():
     """
     Main function to run feature testing
     """
+    conf = config.Config()
+
+    xml_files = helper.list_files(conf.cli_configs_path, ".xml")
