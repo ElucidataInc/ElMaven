2ab3e74db9eacdadf39518d84cd31b4e23ba80ae
Sahil <sahil@elucidata.io>
2017-09-21 17:13:24 +0530

    Ftr: Add output file name to config.json #339
    Removing hard-code parts of code
    
    Issue: #339

--- tests/featureTesting/config/config.py
+++ tests/featureTesting/config/config.py
@@ -27,6 +27,8 @@ class Config(object):
         self.output_path = self.config_data["output_path"]
         self.sample_list = self.config_data["sample_list"]
         self.unique_identifiers = self.config_data["unique_indentifiers"]
+        self.config_extension = self.config_data["config_extension"]
+        self.output_fname = self.config_data["output_fname"]
 
     def get_json_keys(self, json_path):
         """
