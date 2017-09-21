db90aa35fc45a0ba5e49a1ac6eeacba50e625123
Sahil <sahil@elucidata.io>
2017-09-21 17:13:24 +0530

    Ftr: Add output file name to config.json #339
    Removing hard-code parts of code
    
    Issue: #339

--- tests/featureTesting/main.py
+++ tests/featureTesting/main.py
@@ -13,8 +13,7 @@ def main():
     Main function to run feature testing
     """
     conf = config.Config()
-
-    config_files = helper.list_files(conf.cli_configs_path, ".xml")
+    config_files = helper.list_files(conf.cli_configs_path, conf.config_extension)
 
     for config_file in config_files:
         compare_output(config_file)
@@ -35,7 +34,7 @@ def compare_output(config_path):
     cli.run_cli(config_path)
 
     file_list = []
-    file_list.append(os.path.join(output_dir, "compounds.csv"))
+    file_list.append(os.path.join(output_dir, conf.output_fname))
     file_list.append(os.path.splitext(config_path)[0] + ".csv")
     comp = compare.CompareOutput(file_list, os.path.basename(config_path))
     comp.compare()
