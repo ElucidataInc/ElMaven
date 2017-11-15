fa865e19a0db4a47069cfd85cd43fac49d0949cb
Rishabh Gupta <rishabh9511@gmail.com>
2017-11-15 14:33:27 +0530

    Fix: reindent Mavenparameters header file

--- src/core/libmaven/mavenparameters.h
+++ src/core/libmaven/mavenparameters.h
@@ -16,234 +16,235 @@
 
 class Classifier;
 
-class MavenParameters {
-public:
-	MavenParameters();
-	boost::signals2::signal< void (const string&,unsigned int , int ) > sig;
+class MavenParameters 
+{
+    public:
+        MavenParameters();
+        boost::signals2::signal< void (const string&,unsigned int , int ) > sig;
 
-	/**
-	 * [set Output Directory]
-	 * @method setOutputDir
-	 * @param  outdir       [name of the output directory]
-	 */
-	void setOutputDir(QString outdir) {
-		outputdir = outdir.toStdString() + string(DIR_SEPARATOR_STR);
-	}
+        /**
+        * [set Output Directory]
+        * @method setOutputDir
+        * @param  outdir       [name of the output directory]
+        */
+        void setOutputDir(QString outdir) {
+            outputdir = outdir.toStdString() + string(DIR_SEPARATOR_STR);
+        }
 
-	/**
-	 * [set Maximum Group Count]
-	 * @method setMaxGroupCount
-	 * @param  x                [Maximum Group Count]
-	 */
-	void setMaxGroupCount(int x) {
-		limitGroupCount = x;
-	}
-	
-	int getCharge(Compound* compound = NULL);
+        /**
+        * [set Maximum Group Count]
+        * @method setMaxGroupCount
+        * @param  x                [Maximum Group Count]
+        */
+        void setMaxGroupCount(int x) {
+            limitGroupCount = x;
+        }
 
-	/**
-	 * [set Compounds]
-	 * @method setCompounds
-	 * @param  set          [vector of pointer to Compound]
-	 */
-	void setCompounds(vector<Compound*> set) {
-		compounds = set;
-	}
+        int getCharge(Compound* compound = NULL);
 
-	/**
-	 * [set mass Slices]
-	 * @method setSlices
-	 * @param  set       [vector of pointer to mzSlice]
-	 */
-	void setSlices(vector<mzSlice*> set) {
-		_slices = set;
-	}
+        /**
+        * [set Compounds]
+        * @method setCompounds
+        * @param  set          [vector of pointer to Compound]
+        */
+        void setCompounds(vector<Compound*> set) {
+            compounds = set;
+        }
 
-	/**
-	 * [set Peak Group]
-	 * @method setPeakGroup
-	 * @param  p            [pointer to Peak Group]
-	 */
-	void setPeakGroup(PeakGroup* p) {
-		_group = p;
-	}
+        /**
+        * [set mass Slices]
+        * @method setSlices
+        * @param  set       [vector of pointer to mzSlice]
+        */
+        void setSlices(vector<mzSlice*> set) {
+            _slices = set;
+        }
 
-	/**
-	 * [set Average Scan Time]
-	 * @method setAverageScanTime
-	 */
-	void setAverageScanTime();
+        /**
+        * [set Peak Group]
+        * @method setPeakGroup
+        * @param  p            [pointer to Peak Group]
+        */
+        void setPeakGroup(PeakGroup* p) {
+            _group = p;
+        }
 
-	/**
-	 * [set Ionization Mode]
-	 * @method setIonizationMode
-	 */
-	void setIonizationMode();
+        /**
+        * [set Average Scan Time]
+        * @method setAverageScanTime
+        */
+        void setAverageScanTime();
 
-	/**
-	 * [set Samples]
-	 * @method setSamples
-	 * @param  set        [pointer to vector of pointer to mzSample]
-	 */
-	void setSamples(vector<mzSample*>&set);
+        /**
+        * [set Ionization Mode]
+        * @method setIonizationMode
+        */
+        void setIonizationMode();
 
-	/**
-	 * [cleanup all groups]
-	 * @method cleanup
-	 */
-	void cleanup();
-	vector<mzSample*> getVisibleSamples();
+        /**
+        * [set Samples]
+        * @method setSamples
+        * @param  set        [pointer to vector of pointer to mzSample]
+        */
+        void setSamples(vector<mzSample*>&set);
 
-	bool writeCSVFlag;
-	bool alignSamplesFlag;
-	bool keepFoundGroups;
-	bool processAllSlices;
-	bool pullIsotopesFlag;
-	bool showProgressFlag;
-	bool matchRtFlag;
-	bool checkConvergance;
+        /**
+        * [cleanup all groups]
+        * @method cleanup
+        */
+        void cleanup();
+        vector<mzSample*> getVisibleSamples();
 
-	/**
-	 * default ionization mode used by mass spec
-	 */
-	int ionizationMode;
-	int charge;
-	bool formulaFlag = false;
+        bool writeCSVFlag;
+        bool alignSamplesFlag;
+        bool keepFoundGroups;
+        bool processAllSlices;
+        bool pullIsotopesFlag;
+        bool showProgressFlag;
+        bool matchRtFlag;
+        bool checkConvergance;
 
-	// For quantile intensity and qualityWeight
-	double quantileQuality;
-	double quantileIntensity;
-	double quantileSignalBaselineRatio;
-	double quantileSignalBlankRatio;
+        /**
+        * default ionization mode used by mass spec
+        */
+        int ionizationMode;
+        int charge;
+        bool formulaFlag = false;
 
-	//mass slicing parameters
-	float mzBinStep;
-	float rtStepSize;
-	float avgScanTime;
-	MassCutoff *massCutoffMerge;
+        // For quantile intensity and qualityWeight
+        double quantileQuality;
+        double quantileIntensity;
+        double quantileSignalBaselineRatio;
+        double quantileSignalBlankRatio;
 
-	//peak detection
+        //mass slicing parameters
+        float mzBinStep;
+        float rtStepSize;
+        float avgScanTime;
+        MassCutoff *massCutoffMerge;
 
-	/**
-	 * smoothing window
-	 */
-	int eic_smoothingWindow;
-	int eic_smoothingAlgorithm;
-	int baseline_smoothingWindow;
-	int baseline_dropTopX;
+        //peak detection
 
-	double minSignalBaselineDifference;
-	double isotopicMinSignalBaselineDifference;
+        /**
+        * smoothing window
+        */
+        int eic_smoothingWindow;
+        int eic_smoothingAlgorithm;
+        int baseline_smoothingWindow;
+        int baseline_dropTopX;
 
-	int eicType;
+        double minSignalBaselineDifference;
+        double isotopicMinSignalBaselineDifference;
 
-	//peak filtering
-	int minGoodGroupCount;
-	float minSignalBlankRatio;
-	float minNoNoiseObs;
-	float minSignalBaseLineRatio;
-	float minGroupIntensity;
-	int peakQuantitation;
+        int eicType;
 
-	// Peak Group Rank
-	int qualityWeight;
-	int intensityWeight;
-	int deltaRTWeight;
-	bool deltaRtCheckFlag;
+        //peak filtering
+        int minGoodGroupCount;
+        float minSignalBlankRatio;
+        float minNoNoiseObs;
+        float minSignalBaseLineRatio;
+        float minGroupIntensity;
+        int peakQuantitation;
 
-	/**
-	 * eic window around compound, compound detection setting
-	 */
-	MassCutoff *compoundMassCutoffWindow;
-	float compoundRTWindow;
-	int eicMaxGroups;
+        // Peak Group Rank
+        int qualityWeight;
+        int intensityWeight;
+        int deltaRTWeight;
+        bool deltaRtCheckFlag;
 
-	/**
-	 * grouping of peaks across samples
-	 * do no group peaks that are greater than differ more than X in retention time
-	 */
-	float grouping_maxRtWindow;
+        /**
+        * eic window around compound, compound detection setting
+        */
+        MassCutoff *compoundMassCutoffWindow;
+        float compoundRTWindow;
+        int eicMaxGroups;
 
-	/**
-	 * stop looking for groups if group count is greater than X
-	 */
-	int limitGroupCount;
+        /**
+        * grouping of peaks across samples
+        * do no group peaks that are greater than differ more than X in retention time
+        */
+        float grouping_maxRtWindow;
 
-	/**
-	 * triple quad compound matching Q1
-	 */
-	float amuQ1;
+        /**
+        * stop looking for groups if group count is greater than X
+        */
+        int limitGroupCount;
 
-	/**
-	 * triple quad compound matching Q3
-	 */
-	float amuQ3;
+        /**
+        * triple quad compound matching Q1
+        */
+        float amuQ1;
 
-	string filterline;
+        /**
+        * triple quad compound matching Q3
+        */
+        float amuQ3;
 
-	float minQuality;
-	string ligandDbFilename;
+        string filterline;
 
-	double maxIsotopeScanDiff;
-	double maxNaturalAbundanceErr;
-	double minIsotopicCorrelation;
-	int noOfIsotopes;
-	bool C13Labeled_BPE;
-	bool N15Labeled_BPE;
-	bool S34Labeled_BPE;
-	bool D2Labeled_BPE;
-	
-	bool C13Labeled_Barplot;
-	bool N15Labeled_Barplot;
-	bool S34Labeled_Barplot;
-	bool D2Labeled_Barplot;
-    
-	bool C13Labeled_IsoWidget;
-	bool N15Labeled_IsoWidget;
-	bool S34Labeled_IsoWidget;
-	bool D2Labeled_IsoWidget;
+        float minQuality;
+        string ligandDbFilename;
+
+        double maxIsotopeScanDiff;
+        double maxNaturalAbundanceErr;
+        double minIsotopicCorrelation;
+        int noOfIsotopes;
+        bool C13Labeled_BPE;
+        bool N15Labeled_BPE;
+        bool S34Labeled_BPE;
+        bool D2Labeled_BPE;
+
+        bool C13Labeled_Barplot;
+        bool N15Labeled_Barplot;
+        bool S34Labeled_Barplot;
+        bool D2Labeled_Barplot;
+
+        bool C13Labeled_IsoWidget;
+        bool N15Labeled_IsoWidget;
+        bool S34Labeled_IsoWidget;
+        bool D2Labeled_IsoWidget;
 
 
-	float minRt;
-    float maxRt;
-    float minMz;
-    float maxMz;
-    float minIntensity;
-    float maxIntensity;
-    float minCharge;
-    float maxCharge;
+        float minRt;
+        float maxRt;
+        float minMz;
+        float maxMz;
+        float minIntensity;
+        float maxIntensity;
+        float minCharge;
+        float maxCharge;
 
-    string outputdir;
+        string outputdir;
 
-    vector<PeakGroup> allgroups;
-	vector<PeakGroup> undoAlignmentGroups;
-	int alignButton;
-    MassCalculator mcalc;
-    Classifier* clsf;
-	PeakGroup* _group;
-	vector<mzSample*> samples;
-	vector<Compound*> compounds;
-	vector<mzSlice*> _slices;
-	map <string, bool> isotopeAtom;
-	bool stop;
+        vector<PeakGroup> allgroups;
+        vector<PeakGroup> undoAlignmentGroups;
+        int alignButton;
+        MassCalculator mcalc;
+        Classifier* clsf;
+        PeakGroup* _group;
+        vector<mzSample*> samples;
+        vector<Compound*> compounds;
+        vector<mzSlice*> _slices;
+        map <string, bool> isotopeAtom;
+        bool stop;
 
-	int alignMaxItterations; //TODO: Sahil - Kiran, Added while merging mainwindow
-	int alignPolynomialDegree; //TODO: Sahil - Kiran, Added while merging mainwindow
+        int alignMaxItterations; //TODO: Sahil - Kiran, Added while merging mainwindow
+        int alignPolynomialDegree; //TODO: Sahil - Kiran, Added while merging mainwindow
 
-    float minFragmentMatchScore;
-    bool matchFragmentation;
-    MassCutoff* fragmentMatchMassCutoffTolr;
-    /**
-     * [print parameter Settings]
-     * @method printSettings
-     */
-    void printSettings();
+        float minFragmentMatchScore;
+        bool matchFragmentation;
+        MassCutoff* fragmentMatchMassCutoffTolr;
+        /**
+        * [print parameter Settings]
+        * @method printSettings
+        */
+        void printSettings();
 
-	//options dialog::peak grouping tab-widget
-	double distXWeight;
-	double distYWeight;
-	double overlapWeight;
-	bool useOverlap;
+        //options dialog::peak grouping tab-widget
+        double distXWeight;
+        double distYWeight;
+        double overlapWeight;
+        bool useOverlap;
 
 };
 
