a9d64e8d3e01a7a310b49eaf7aa9dd4a0ffbe3f4
Deepak Sharma <deepak.sharma@elucidata.io>
2018-01-03 22:04:23 +0530

    [ADD]: Added constants for feature testing

---
+++ tests/featureTesting/constants_feature_testing.py
@@ -0,0 +1,9 @@
+CLI_CONFIGS_PATH = "data/"
+CONFIG_EXTENSION = "xml"
+PEAK_DETECTOR_PATH = "../../../build/bin/peakdetector"
+OUTPUT_PATH = "tmp/"
+RESULT_DIR = "results/"
+PLOT_RESULT = "Results.html"
+OUTPUT_FILE_NAME = "compounds.csv"
+UNIQUE_IDENTIFIERS = ["compoundId", "compound", "formula", "goodPeakCount"]
+SAMPLE_LIST = ["testsample_1", "testsample_2", "testsample_3"]
