a9d0f718c4d7d8c3fc4181cc772f53062299dbf6
Deepak Sharma <deepak.sharma@elucidata.io>
2018-01-03 18:57:20 +0530

    [FTR]: Added feature to compare two method files

---
+++ tests/featureTesting/compare_parameters_file.py
@@ -0,0 +1,28 @@
+from helper import helper_functions as hf
+
+
+class CompareParameters(object):
+    def __init__(self, file_1_path, file_2_path):
+        self.file_1_path = file_1_path
+        self.file_2_path = file_2_path
+
+    def parameters_comparison(self):
+        """
+            This function first reads the two method files (xml files) for both (for example: manual
+             and automated curated datasets) the datasets and returns the parameters which
+             are different in both the files as a dict. This dict has parameter names which has
+             different values in both the datasets as key and their respective
+             values in a list for that key.
+
+            :param file1_path: file path of dataset one
+
+            :param file2_path: file path of the other dataset
+
+            :return: A dict of parameters which are different in between both the datsets.
+            for example: {'compoundRTWindow': ['0.5', '0.3'],
+            'baseline_smoothingWindow': ['6', '4'], 'minSignalBlankRatio': ['0', '3'],
+            'maxRt': ['20', '2'], 'ppmMerge': ['20', '2']}
+
+            """
+        param_comp = hf.dict_diff(self.file_1_path, self.file_2_path)
+        return param_comp
