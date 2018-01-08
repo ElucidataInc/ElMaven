f75540b9a328ec07036dc0c8f6570b194a2fc649
Deepak Sharma <deepak.sharma@elucidata.io>
2018-01-08 16:03:58 +0530

    [ADD]:Added constants

--- tests/featureTesting/constants_feature_testing.py
+++ tests/featureTesting/constants_feature_testing.py
@@ -1,9 +1,10 @@
 CLI_CONFIGS_PATH = "data/"
 CONFIG_EXTENSION = "xml"
-PEAK_DETECTOR_PATH = "../../../build/bin/peakdetector"
+PEAK_DETECTOR_PATH = "../../bin/peakdetector.app/Contents/MacOS/peakdetector"
 OUTPUT_PATH = "tmp/"
 RESULT_DIR = "results/"
 PLOT_RESULT = "Results.html"
-OUTPUT_FILE_NAME = "compounds.csv"
+OUTPUT_FILE_NAME_CSV = "compounds.csv"
 UNIQUE_IDENTIFIERS = ["compoundId", "compound", "formula", "goodPeakCount"]
 SAMPLE_LIST = ["testsample_1", "testsample_2", "testsample_3"]
+OUTPUT_FILE_NAME_JSON = "compounds.json"
