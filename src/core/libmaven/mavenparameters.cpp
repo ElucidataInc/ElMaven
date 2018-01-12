02ebdad87726843bef7e212722962e530a50ae73
IpankajI <pankaj.kumar@elucidata.io>
2018-01-12 13:39:19 +0530

    Fix: set compoundRtWindow 1 as default #514
    	While doing alignment w.r.t expectedRt, expected rt values
    be from original data set uploaded not from any other compound database,
    otherwise there won't be good alignment. And compoundRtWindow should
    also have a reasonable value (round 1).
    
    Issue: #514

--- src/core/libmaven/mavenparameters.cpp
+++ src/core/libmaven/mavenparameters.cpp
@@ -77,7 +77,7 @@ MavenParameters::MavenParameters(string settingsPath):lastUsedSettingsPath(setti
         */
         compoundMassCutoffWindow=new MassCutoff();
         compoundMassCutoffWindow->setMassCutoff(10);
-        compoundRTWindow = 2;
+        compoundRTWindow = 1;
         eicMaxGroups = INT_MAX;
 
         amuQ1 = 0.5;
