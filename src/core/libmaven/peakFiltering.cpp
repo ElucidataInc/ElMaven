a92ae3d2c9e31ee29f99310bb70a953099c45b8e
Sahil <sahil@elucidata.io>
2018-01-17 14:56:17 +0530

    Ftr: Add func to filter peaks in vector of EICs #516
    Issue: #516

--- src/core/libmaven/peakFiltering.cpp
+++ src/core/libmaven/peakFiltering.cpp
@@ -5,6 +5,16 @@ PeakFiltering::PeakFiltering(MavenParameters *mavenParameters)
     _mavenParameters = mavenParameters;
 }
 
+void PeakFiltering::filter(vector<EIC*> &eics)
+{
+
+    for (unsigned int i = 0; i < eics.size(); i++)
+    {
+        filter(eics[i]->peaks);
+    }
+
+}
+
 void PeakFiltering::filter(vector<Peak> &peaks)
 {
 
