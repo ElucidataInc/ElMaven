b19f6c8cf227ed4979bd85acd4ec24fff2878e7e
Deepak Sharma <deepak.sharma@elucidata.io>
2018-01-22 18:24:52 +0530

    [ADD]: Updated run.sh file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 OMP_CANCELLATION=true
-export QT_SELECT=qt5 
+export QT_SELECT=qt5
 flag=100   #this flag check wether qt running in debug mode or not
 while true; do
     read -p "Do you wish to install this program in release mode? " yn
@@ -11,16 +11,6 @@ while true; do
 done
 
 make -j $(getconf _NPROCESSORS_ONLN)
-
-if [ "$(uname)" == "Darwin" ]; then
-      make -j $(sysctl -n hw.ncpu)
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    make -j $(nproc)
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    make -j $(nproc)
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    make -j $(nproc)
-fi
 
 if [ -f tests/MavenTests/test.xml ]; then
 	rm test*.xml
