187fd0ef20e95eae3f65c31e83e7a381bedf0443
IpankajI <pankaj.kumar@elucidata.io>
2018-01-24 17:22:14 +0530

    Fix: use QCollator to sort text with numbers #566

--- src/gui/mzroll/numeric_treewidgetitem.h
+++ src/gui/mzroll/numeric_treewidgetitem.h
@@ -21,31 +21,12 @@ public:
 	bool operator< ( const QTreeWidgetItem & other ) const  
 	{
 		int sortCol = treeWidget()->sortColumn();
-		bool isaFloat;
-		bool isbFloat;
-		float a = text(sortCol).toFloat(&isaFloat);
-		float b = other.text(sortCol).toFloat(&isbFloat);
-
-		if (isaFloat & isbFloat) {
-			return  a < b;
-		}
-		else if(a){
-			return true;
-		}
-		else if(b){
-			return false;
-		}
-
-
-		QString text1=text(sortCol);
-		QString text2=other.text(sortCol);
-		int length1=text1.length();
-		int length2=text2.length();
-
-		if(length1==length2) {
-			return text1<text2;
-		}
-		else return length1<length2;
+		QString thisText=text(sortCol);
+		QString otherText=other.text(sortCol);
+		
+		QCollator collator;
+		collator.setNumericMode(true);
+		return collator.compare(thisText , otherText) < 0;
 
     }
 
