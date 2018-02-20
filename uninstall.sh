8161bda92c7aa064ec1fb7ce5c12b99b9077d610
IpankajI <pankaj.kumar@elucidata.io>
2018-02-20 17:50:17 +0530

    Fix: update run.sh and uninstall.sh 	While compiling in debug mode, run.sh will run MavenTests and will profile whole project. At last it will run lcov and generates one directory coverage in parent directory of ElMaven and one file coverage.info. Folder coverage will have all test coverage info. To see test coverage, click on index.html in coverage folder

--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,6 @@ rm -rf build/lib/*
 make -C MavenTests/ distclean
 rm -rf bin/maven_dev* bin/El_Maven* bin/peakdetector bin/mzWatcher docs/html docs/latex 
 rm -rf $HOME/.config/mzRoll
+find . -type d | grep tmp | xargs rm -rfd
+rm -frd ../coverage
+rm -rf ../coverage.info
