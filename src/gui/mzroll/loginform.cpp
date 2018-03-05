a15a1c3ef17efe06b1f1c3b7959b79e6b9847f48
kyelucidata <kailash.yadav@elucidata.io>
2018-03-05 14:11:33 +0530

    changing Form class from QWidget to QDialog and modifying login label on click now..

--- src/gui/mzroll/loginform.cpp
+++ src/gui/mzroll/loginform.cpp
@@ -4,9 +4,10 @@
 
 
 LoginForm::LoginForm(MainWindow *parent,PollyIntegration* pollyintegration) :
-    QWidget(0),
+    QDialog(parent),
     ui(new Ui::LoginForm)
 {
+
     ui->setupUi(this);
     _mainwindow=parent;
     _pollyintegration = pollyintegration;
@@ -24,25 +25,17 @@ void LoginForm::on_pushButton_clicked()
     int status_inside;
     username = ui->lineEdit_username->text();
     password = ui->lineEdit_password->text();
-    qDebug()<<"username and password -  "<<username <<"   "<<password;
+    ui->login_label->setStyleSheet("QLabel {color : green; }");
+    ui->login_label->setText("Logging into polly..");
     status_inside = _pollyintegration->authenticate_login(username,password);
     if (status_inside==1){
-        // w->hide();
-        QMessageBox::information(this,"Login","username and password are correct..logged in");
-        qDebug() << "logged in, you can now send data to polly....\n\n";
         hide();
-        qDebug() << "popup deleted....\n\n";
         _mainwindow->check_polly_login->setText("connected");
         _mainwindow->check_polly_login->setStyleSheet("QLabel { background-color : white; color : green; }");
-        // status=1;
-        qDebug() << "status changed....\n\n";
         _pollyintegration->get_project_name();
-        qDebug() << "logged in, data sent to polly....\n\n";
-        
-        
     }
     else{
-        QMessageBox::information(this,"Login","username and password are not correct");
+        ui->login_label->setStyleSheet("QLabel {color : red; }");
+        ui->login_label->setText("Incorrect credentials");
     }
-    qDebug()<<"exiting on_pushButton_clicked now.."<<status_inside;
 }
