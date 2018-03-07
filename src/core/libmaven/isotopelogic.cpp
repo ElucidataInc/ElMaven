b1ab6f7783afecd8203a14ca3a3483ab27855132
shubhra-agrawal <shubhra.agrawal@elucidata.io>
2018-03-07 13:45:54 +0000

    Ref: Remove redundant function from isotopeLogic

--- src/core/libmaven/isotopelogic.cpp
+++ src/core/libmaven/isotopelogic.cpp
@@ -17,24 +17,3 @@ void IsotopeLogic::userChangedFormula() {
 	_compound = tempCompound;
 }
 
-float IsotopeLogic::getIsotopeIntensity(float mz, MassCutoff *massCutoff) {
-	float highestIntensity = 0;
-
-	if (_scan == NULL)
-		return 0;
-	mzSample* sample = _scan->getSample();
-	if (sample == NULL)
-		return 0;
-
-	for (int i = _scan->scannum - 2; i < _scan->scannum + 2; i++) {
-		Scan* s = sample->getScan(i);
-		vector<int> matches = s->findMatchingMzs(mz - massCutoff->massCutoffValue(mz),
-				mz + massCutoff->massCutoffValue(mz));
-		for (unsigned int i = 0; i < matches.size(); i++) {
-			int pos = matches[i];
-			if (s->intensity[pos] > highestIntensity)
-				highestIntensity = s->intensity[pos];
-		}
-	}
-	return highestIntensity;
-}
