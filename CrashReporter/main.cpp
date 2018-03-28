9fa898c4b4ce7716414d7da18d676440f036d42e
Rishabh Gupta <rishabh9511@gmail.com>
2018-03-28 09:52:42 +0530

    win_fix: make Error handling objects build inside build/tmp/Error_hanlding
    - start crash reporter start without any arguments

--- CrashReporter/main.cpp
+++ CrashReporter/main.cpp
@@ -10,15 +10,7 @@ int main(int argc, char *argv[])
 
     MainWindow w;
 
-    w.restartApplicationPath = QString(argv[1]);
-    w.bucketName = QString(argv[2]);
-    w.accessKey = QString(argv[3]);
-    w.secretKey = QString(argv[4]);
-    w.logInformation = QString(argv[5]);
-    w.windowState = QString(argv[6]).toInt();
-
     w.show();
-    w.onStart();
 
     return a.exec();
 }
