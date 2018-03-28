3a10a445db17c96c7aa0e0c4f72c7231b8041bc6
Pankaj Kumar <pankaj.kumar@elucidata.io>
2018-03-28 19:00:08 +0530

    Ftr: setReferenceData will set data of a samples, which will be used to align other sample retention times. 	alignRt will currect retention times of given samples w.r.t set data

--- src/core/obiwarp/obiwarp.cpp
+++ src/core/obiwarp/obiwarp.cpp
@@ -1,12 +1,62 @@
 #include "obiwarp.h"
-#include <iostream>
+
 
 ObiWarp::ObiWarp(){
     std::cerr<<"===========mission successful============"<<std::endl;
 }
 ObiWarp::~ObiWarp(){
-    
+    //release memory from all places
 }
+
+void ObiWarp::setReferenceData(vector<float> &rtPoints, vector<float> &mzPoints, vector<vector<float> >& intMat){
+    _tm_vals = rtPoints.size();
+    float* tmPoint = new float[_tm_vals];
+    for(int i=0; i < _tm_vals ; ++i)
+        tmPoint[i] = rtPoints[i];
+    _tm.take(_tm_vals, tmPoint);
+
+    _mz_vals = mzPoints.size();
+    float* mzPoint = new float[_mz_vals];
+    for(int i = 0; i < _mz_vals; ++i)
+        mzPoint[i] = mzPoints[i];
+    _mz.take(_mz_vals, mzPoint);
+
+    assert(_tm_vals == intMat.size());
+    MatF mat(_tm_vals, _mz_vals);
+    for(int i=0; i < _tm_vals; ++i){
+        assert(_mz_vals == intMat[i].size());
+        for(int j = 0; j < _mz_vals; ++j)
+            mat(i,j) = intMat[i][j];
+    }
+    _mat.take(mat);
+}
+
+vector<float>& alignRt(vector<float> &rtPoints, vector<float> &mzPoints, vector<vector<float> >& intMat){
+    VecF tm;
+    int tm_vals = rtPoints.size();
+    float* tmPoint = new float[tm_vals];
+    for(int i = 0; i < tm_vals; ++i)
+        tmPoint[i] = rtPoints[i];
+    tm.take(tm_vals, tmPoint);
+
+    VecF mz;
+    int mz_vals = mzPoints.size();
+    float* mzPoint = new float[mz_vals];
+    for(int i = 0; i < mz_vals; ++i)
+        mzPoint[i] = mzPoints[i];
+    mz.take(mz_vals, mzPoint);
+
+    assert(tm_vals = intMat.size());
+    MatF mat(tm_vals, mz_vals);
+    for(int i = 0; i < tm_vals; ++i){
+        assert(mz_vals == intMat[i].size());
+        for(int j = 0; j < mz_vals; ++j)
+            mat(i,j)=intMat[i][j];
+    }
+
+}
+
+
 void ObiWarp::tm_axis_vals(VecI &tmCoords, VecF &tmVals,VecF &_tm ,int _tm_vals){
 
 }
--- src/core/obiwarp/obiwarp.h
+++ src/core/obiwarp/obiwarp.h
@@ -1,17 +1,34 @@
 #ifndef OBIWARP_H
 #define OBIWARP_H
 
+#include <vector>
+#include <iostream>
+#include <assert.h>
+
 #include "vec.h"
 #include "mat.h"
 #include "dynprog.h"
+
+using namespace std;
 
 class ObiWarp{
 public:
     ObiWarp();
     ~ObiWarp();
+    void setReferenceData(vector<float> &rtPoints, vector<float> &mzPoints, vector<vector<float> >& intMat);
+    vector<float>& alignRt(vector<float> &rtPoints, vector<float> &mzPoints, vector<vector<float> >& intMat);
 private:
     void tm_axis_vals(VecI &tmCoords, VecF &tmVals,VecF &_tm ,int _tm_vals);
     void warp_tm(VecF &selfTimes, VecF &equivTimes, VecF &_tm);
+    VecF _tm;
+    VecF _mz;
+    MatF _mat;
+    int _tm_vals;
+    int _mz_vals;
+    DynProg dyn;
+
+
+
 
 };
 
