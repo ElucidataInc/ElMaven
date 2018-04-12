9e8e9ebf40f085c3db590d82d165016e327b6ece
Pankaj Kumar <pankaj.kumar@elucidata.io>
2018-04-12 11:22:07 +0530

    Fix: Add peak-position according to update rt #550 	In case of alignment of, rt-value of scan is updated. But peak- position is added according of rt-variable of calss peak which is duplicate of rt of scan. 	Now peak-position is added according to rt-value of scan which is what always intended to be used
    Issue: #550

--- src/gui/mzroll/eicwidget.cpp
+++ src/gui/mzroll/eicwidget.cpp
@@ -1294,9 +1294,11 @@ void EicWidget::addPeakPositions(PeakGroup* group) {
 
 		if (peak.getSample() != NULL && peak.getSample()->isSelected == false)
 			continue;
+		
+		float& rt = peak.getSample()->scans[peak.scan]->rt;
 		if (eicParameters->_slice.rtmin != 0 && eicParameters->_slice.rtmax != 0
-				&& (peak.rt < eicParameters->_slice.rtmin
-						|| peak.rt > eicParameters->_slice.rtmax))
+				&& (rt < eicParameters->_slice.rtmin
+						|| rt > eicParameters->_slice.rtmax))
 			continue;
 
 		QColor color = Qt::black;
@@ -1310,7 +1312,7 @@ void EicWidget::addPeakPositions(PeakGroup* group) {
 		QBrush brush(color);
 		brush.setStyle(Qt::NoBrush);
 
-		EicPoint* p = new EicPoint(toX(peak.rt), toY(peak.peakIntensity), &peak,
+		EicPoint* p = new EicPoint(toX(rt), toY(peak.peakIntensity), &peak,
 				getMainWindow());
 		if (setZValue)
 			p->setZValue(i);
