5810cc35027477b877a7aacc0b6b4b536fccdfaf
kyelucidata <kailash.yadav@elucidata.io>
2018-04-30 18:53:57 +0530

    [WIP] logout function addded+better error handelling while connecting to polly

--- src/pollyCLI/pollyintegration.h
+++ src/pollyCLI/pollyintegration.h
@@ -9,20 +9,21 @@ class PollyIntegration
 	public:
 	    ~PollyIntegration();
 	    PollyIntegration();
+		void logout();
 	    QString username;
 	    QString password;
 		QString jsPath;
 		QString nodePath;
 		QString user_login_required();
 		QString createProjectOnPolly(QString projectname);
-        QByteArray run_qt_process(QString command, QStringList args = QStringList());
+        QByteArrayList run_qt_process(QString command, QStringList args = QStringList());
 	    QByteArray run_system_process(QString command);
 	    QString get_urls(QByteArray result);
 	    QStringList get_system_urls(QString filename);
 	    QStringList get_project_upload_url_commands(QByteArray result2,QStringList filenames);
 		QStringList get_projectFiles_download_url_commands(QByteArray result2,QStringList filenames);
 	    QString get_run_id(QByteArray result);
-	    int authenticate_login(QString username,QString password);
+	    QString authenticate_login(QString username,QString password);
 	    int check_already_logged_in();
 		int check_node_executable();
 	    QStringList exportData(QStringList filenames,QString projectId);
