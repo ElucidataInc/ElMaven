9505a9eac403e9878d7ad26ac04d58a038d5d3da
kyelucidata <kailash.yadav@elucidata.io>
2018-05-08 19:32:37 +0530

    [WIP] showing the UI only when the data is ready..

--- src/gui/mzroll/mainwindow.cpp
+++ src/gui/mzroll/mainwindow.cpp
@@ -2726,7 +2726,7 @@ void MainWindow::showPeakdetectionDialog() {
 }
 
 void MainWindow::showPollyElmavenInterfaceDialog() {
-	pollyElmavenInterfaceDialog->show();
+	// pollyElmavenInterfaceDialog->show();
 	pollyElmavenInterfaceDialog->initialSetup();
 }
 
