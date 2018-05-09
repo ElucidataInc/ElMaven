090ea6d48706cdc934b4c5a65db37661b2dbba20
Sahil <sahil@elucidata.io>
2018-05-09 17:50:47 +0530

    [Add] versioning mzroll file format
    - This will help manage backward compatability better in mzroll file

--- src/gui/mzroll/projectdockwidget.h
+++ src/gui/mzroll/projectdockwidget.h
@@ -63,6 +63,7 @@ private Q_SLOTS:
     void filterTreeItems(QString filterString);
 
 private:
+    float mzrollVersion;
     QTreeWidgetItem* getParentFolder(QString filename);
     void markBlank(QTreeWidgetItem* item);
     void unmarkBlank(QTreeWidgetItem* item);
