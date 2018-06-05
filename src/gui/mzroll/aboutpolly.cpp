4d835bd0a35cc7bff14d16a8d2795c4df3c1e48b
kyelucidata <kailash.yadav@elucidata.io>
2018-06-05 17:49:08 +0530

    [WIP] adding image to the info tab

--- src/gui/mzroll/aboutpolly.cpp
+++ src/gui/mzroll/aboutpolly.cpp
@@ -2,7 +2,6 @@
 #include "ui_aboutpolly.h"
 #include <QMessageBox>
 
-
 AboutPolly::AboutPolly() :
     QDialog(),
     ui(new Ui::AboutPolly)
@@ -18,6 +17,10 @@ AboutPolly::AboutPolly() :
     ui->label_visit_polly->setTextFormat(Qt::RichText);
     ui->label_visit_polly->setTextInteractionFlags(Qt::TextBrowserInteraction);
     ui->label_visit_polly->setOpenExternalLinks(true);
+    QPixmap pix( ":/images/POLLY.png");
+    ui->label_polly_logo->setPixmap(pix);
+    ui->label_polly_logo->setGeometry(220,10, 61,51);
+
 }
 
 AboutPolly::~AboutPolly()
