19de4dd2bdc8aa865043c49f059a604c0fa06fb8
Shubhra Agrawal <shubhra.agrawal@elucidata.io>
2018-06-06 19:02:25 +0530

    [UI] Change "About Polly" page

--- src/gui/mzroll/aboutpolly.cpp
+++ src/gui/mzroll/aboutpolly.cpp
@@ -8,19 +8,11 @@ AboutPolly::AboutPolly() :
     
 {
     ui->setupUi(this);
-    ui->label_video_link->setText("<a href=\"https://www.youtube.com/watch?v=tfxksBKWTS0\">Video link</a>");
-    ui->label_video_link->setTextFormat(Qt::RichText);
-    ui->label_video_link->setTextInteractionFlags(Qt::TextBrowserInteraction);
-    ui->label_video_link->setOpenExternalLinks(true);
 
-    ui->label_visit_polly->setText("<a href=\"https://polly.elucidata.io\">Visit our website:Polly</a>");
-    ui->label_visit_polly->setTextFormat(Qt::RichText);
-    ui->label_visit_polly->setTextInteractionFlags(Qt::TextBrowserInteraction);
-    ui->label_visit_polly->setOpenExternalLinks(true);
-    QPixmap pix( ":/images/POLLY.png");
-    ui->label_polly_logo->setPixmap(pix);
-    ui->label_polly_logo->setGeometry(220,10, 61,51);
+    ui->label_3->setOpenExternalLinks(true);
+    ui->label_2->setOpenExternalLinks(true);
 
+    setWindowTitle("About Polly");
 }
 
 AboutPolly::~AboutPolly()
@@ -29,6 +21,5 @@ AboutPolly::~AboutPolly()
 }
 
 void AboutPolly::cancel(){
-    qDebug()<<"closing the info tab now..";
     close();
 }
