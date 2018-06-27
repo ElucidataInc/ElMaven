8b3dcba201e401757963b09b286e43577827638b
Sahil <sahil@elucidata.io>
2018-06-27 15:50:10 +0530

    [Fix] Crash while closing the application
    memory was not allocated to pointer ui

--- src/gui/mzroll/pollywaitdialog.cpp
+++ src/gui/mzroll/pollywaitdialog.cpp
@@ -1,6 +1,6 @@
 #include "pollywaitdialog.h"
 
-PollyWaitDialog::PollyWaitDialog(QWidget *parent) : QDialog(parent) {
+PollyWaitDialog::PollyWaitDialog(QWidget *parent) : QDialog(parent), ui(new Ui::PollyWaitDialog) {
     setupUi(this);
     statusLabel->setStyleSheet("QLabel {color : green; }");
     statusLabel->setAlignment(Qt::AlignCenter);
