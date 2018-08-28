8184fb7288c012100b41535662cb611952f73ccf
Shubhra Agrawal <shubhra.agrawal@elucidata.io>
2018-08-28 13:11:16 +0530

    [Fix] Add cohort names to file if available

--- src/gui/mzroll/projectdockwidget.cpp
+++ src/gui/mzroll/projectdockwidget.cpp
@@ -132,7 +132,8 @@ void ProjectDockWidget::prepareSampleCohortFile(QString sampleCohortFileName) {
 	QTextStream out(&file);
 	out << "Sample" << ",Cohort" << "\n";
 	for (const auto& sample : loadedSamples) {
-		out << QString::fromStdString(sample->getSampleName()) << ",\n";
+		out << QString::fromStdString(sample->getSampleName()) << "," << 
+            QString::fromStdString(sample->getSetName()) << "\n";
 	}
 
 	qDebug() << "sample cohort file prepared";
