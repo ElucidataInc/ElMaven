007538a77a731158dcee174832f2e1ccb3ce2b2a
Saiful B. Khan <saifulbkhan@gmail.com>
2019-07-01 11:56:49 +0530

    Make CSV and MSP output consistent

--- src/gui/mzroll/database.cpp
+++ src/gui/mzroll/database.cpp
@@ -597,6 +597,7 @@ int Database::saveNISTLibrary(vector<PeakGroup*> groups,
         if (signal)
             (*signal)("Saving spectral library: " + filepath, written, count);
 
+        fstream << fixed << setprecision(6);
         if (group->compound != nullptr) {
             fstream << "NAME: " << group->compound->name << "\n";
             fstream << "MW: " << group->compound->mass << "\n";
@@ -608,8 +609,8 @@ int Database::saveNISTLibrary(vector<PeakGroup*> groups,
                     << "\n";
         }
 
-        fstream << "RT: " << group->meanRt << "\n";
-        fstream << "PRECURSORMZ: " << group->meanMz << "\n";
+        fstream << setprecision(3) << "RT: " << group->meanRt << "\n";
+        fstream << setprecision(6) << "PRECURSORMZ: " << group->meanMz << "\n";
 
         if (group->compound != nullptr) {
             fstream << "SMILE: " << group->compound->smileString << "\n";
