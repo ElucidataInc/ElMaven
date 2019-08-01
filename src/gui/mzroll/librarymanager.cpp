83aa0fb87cf3ff332814c318a197e4d83de35a0c
Saiful B. Khan <saiful.khan@elucidata.io>
2019-08-01 13:29:22 +0530

    Add utility classes for library save/load

--- src/gui/mzroll/librarymanager.cpp
+++ src/gui/mzroll/librarymanager.cpp
@@ -6,3 +6,130 @@ LibraryManager::LibraryManager(MainWindow* parent)
 {
     setupUi(this);
 }
+
+LibraryRecord::LibraryRecord(const QJsonObject& json)
+{
+    absolutePath = "";
+    databaseName = "";
+    numberOfCompounds = 0;
+
+    if (json.contains("absolutePath") && json["absolutePath"].isString())
+        absolutePath = json["absolutePath"].toString();
+
+    if (json.contains("databaseName") && json["databaseName"].isString())
+        databaseName = json["databaseName"].toString();
+
+    if (json.contains("numberOfCompounds") && json["numberOfCompounds"].isDouble())
+        numberOfCompounds = json["numberOfCompounds"].toInt();
+}
+
+QJsonObject LibraryRecord::toJson() const
+{
+    QJsonObject json;
+    json["absolutePath"] = absolutePath;
+    json["databaseName"] = databaseName;
+    json["numberOfCompounds"] = numberOfCompounds;
+    return json;
+}
+
+void LibraryWorker::saveRecord(const LibraryRecord& record)
+{
+    bool recordAlreadyExists = false;
+    auto entries = _loadEntries();
+    for (size_t i = 0; i < entries.size(); ++i) {
+        QJsonObject entry = entries[i].toObject();
+        LibraryRecord existingRecord(entry);
+        // record having the exact same file path should have the same stats
+        if (existingRecord.absolutePath == record.absolutePath) {
+            existingRecord.databaseName = record.databaseName;
+            existingRecord.numberOfCompounds = record.numberOfCompounds;
+            entries.replace(i, existingRecord.toJson());
+            recordAlreadyExists = true;
+        }
+    }
+
+    if (!recordAlreadyExists)
+        entries.append(record.toJson());
+
+    emit saveFinished(_saveEntries(entries));
+}
+
+void LibraryWorker::deleteRecord(const LibraryRecord& record)
+{
+    QList<size_t> recordsIndicesToBeDeleted;
+    auto entries = _loadEntries();
+    for (size_t i = 0; i < entries.size(); ++i) {
+        QJsonObject entry = entries[i].toObject();
+        LibraryRecord existingRecord(entry);
+        // any record having the exact same file path will be deleted
+        if (existingRecord.absolutePath == record.absolutePath)
+            recordsIndicesToBeDeleted.append(i);
+    }
+
+    if (recordsIndicesToBeDeleted.isEmpty())
+        emit deleteFinished(false);
+
+    for (auto index : recordsIndicesToBeDeleted)
+        entries.removeAt(index);
+
+    emit deleteFinished(_saveEntries(entries));
+}
+
+void LibraryWorker::loadRecords()
+{
+    QList<LibraryRecord> records;
+    auto entries = _loadEntries();
+    for (size_t i = 0; i < entries.size(); ++i) {
+        QJsonObject entry = entries[i].toObject();
+        LibraryRecord record(entry);
+        records.push_back(record);
+    }
+    emit loadFinished(records);
+}
+
+QString LibraryWorker::_libraryStorePath()
+{
+    auto configLocation = QStandardPaths::GenericConfigLocation;
+    auto parentDir = QStandardPaths::writableLocation(configLocation)
+                     + QDir::separator() + "ElMaven";
+
+    // create parent directory if does not already exist
+    if (!QFile::exists(parentDir))
+        QDir().mkpath(parentDir);
+
+    auto libraryPath = QString(parentDir + QDir::separator() + "library.json");
+
+    // create library file if does not already exist
+    if (!QFile::exists(libraryPath))
+        QFile(libraryPath).open(QIODevice::ReadWrite);
+
+    return libraryPath;
+}
+
+bool LibraryWorker::_saveEntries(const QJsonArray& entries)
+{
+    QFile libraryFile(_libraryStorePath());
+    if (!libraryFile.open(QIODevice::WriteOnly)) {
+        cerr << "Error: Could not save to library file" << endl;
+        return false;
+    }
+
+    QJsonDocument libraryDoc(entries);
+    libraryFile.write(libraryDoc.toJson());
+    return false;
+}
+
+QJsonArray LibraryWorker::_loadEntries()
+{
+    QFile libraryFile(_libraryStorePath());
+    if (!libraryFile.open(QIODevice::ReadOnly)) {
+        cerr << "Error: Could not open library file" << endl;
+        return QJsonArray();
+    }
+
+    auto json = QJsonDocument::fromJson(libraryFile.readAll());
+    if (json.isArray())
+        return json.array();
+
+    return QJsonArray();
+}
--- src/gui/mzroll/librarymanager.h
+++ src/gui/mzroll/librarymanager.h
@@ -20,4 +20,126 @@ private:
     MainWindow* _mw;
 };
 
+/**
+ * @brief The LibraryRecord struct contains all attributes of a compound
+ * database that needs to be saved in the library, meant to be shared over
+ * application sessions.
+ */
+struct LibraryRecord {
+    /**
+     * @brief The absolute path of a compound database file on the filesystem.
+     */
+    QString absolutePath;
+
+    /**
+     * @brief Name of the compound database.
+     */
+    QString databaseName;
+
+    /**
+     * @brief The number of compounds stored in this compound database.
+     */
+    int numberOfCompounds;
+
+    /**
+     * @brief Basic constructor.
+     * @param absolutePath Path to be set for this compound database.
+     * @param databaseName Name to be set for this compound database.
+     * @param numberOfCompounds Number of compounds in this compound database.
+     */
+    LibraryRecord(const QString& absolutePath,
+                  const QString& databaseName,
+                  const int numberOfCompounds) :
+                  absolutePath(absolutePath),
+                  databaseName(databaseName),
+                  numberOfCompounds(numberOfCompounds) {}
+
+    /**
+     * @brief Construct a `LibraryRecord` using a JSON object.
+     * @param json A `QJsonObject` that contains values to be set for this
+     * record.
+     */
+    LibraryRecord(const QJsonObject& json);
+
+    /**
+     * @brief Convert a record to an equivalent JSON object.
+     * @return A `QJsonObject` representing the values for this record.
+     */
+    QJsonObject toJson() const;
+};
+
+/**
+ * @brief The LibraryWorker class is meant to be used as a worker that runs in
+ * a separate thread and performs the saving and loading operation for library
+ * records into and from JSON files.
+ */
+class LibraryWorker : public QObject
+{
+    Q_OBJECT
+
+public slots:
+    /**
+     * @brief Save a compound database record in the library.
+     * @details If an entry already exists in the shared library, that has the
+     * same path as the given record, only attributes of the existing entry is
+     * updated to match the given record.
+     * Once the operation completes, the `saveFinished` signal is emitted with
+     * a status value indicating success with `true` and failure with `false`.
+     * @param record A `LibraryRecord` object to be saved in the library.
+     */
+    void saveRecord(const LibraryRecord& record);
+
+    /**
+     * @brief Delete the given record from saved library (JSON) file.
+     * @details After completion of the delete operation, the `deleteFinished`
+     * signal is emitted with a boolean status, where `true` means deletion was
+     * successful and `false` means it failed (or the record never existed).
+     * @param record A `LibraryRecord` for which, all entries having the same
+     * path as this record will be deleted.
+     */
+    void deleteRecord(const LibraryRecord& record);
+
+    /**
+     * @brief Load all records saved in the library.
+     * @details All entries in the library (which itself is a JSON array of
+     * objects), will be converted to `LibraryRecord` objects which will be
+     * packaged in a `QList` and emitted with the `loadFinished` signal, once
+     * the operation completes.
+     */
+    void loadRecords();
+
+signals:
+    void saveFinished(bool status);
+    void deleteFinished(bool status);
+    void loadFinished(const QList<LibraryRecord> records);
+
+private:
+    /**
+     * @brief Obtain the path of the JSON file storing our library of compound
+     * databases.
+     * @details This method also creates the file (and its parent directories),
+     * if it does not already exist.
+     * @return A `QString` set to the path of the on-disk library (JSON) file.
+     */
+    QString _libraryStorePath();
+
+    /**
+     * @brief Utility function that can be used to save a JSON array of DB
+     * entries to the library file.
+     * @param entries A `QJsonArray` object which will be overwritten to the
+     * library file.
+     * @return A boolean status where `true` means save was successful and
+     * `false` means it failed.
+     */
+    bool _saveEntries(const QJsonArray& entries);
+
+    /**
+     * @brief Utility function, that reads the library file and returns the
+     * contents as an JSON array object, if successfully parsed as such.
+     * @return A `QJsonArray` object that has been read from the library file.
+     * If the read or parse operations fail, an empty `QJsonArray` is returned.
+     */
+    QJsonArray _loadEntries();
+};
+
 #endif // LIBRARYMANAGER_H
