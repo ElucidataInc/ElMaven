279d061c77f26d9b51317a40c7b5073ab0d3c360
Saiful B. Khan <saiful.khan@elucidata.io>
2019-09-26 10:15:05 +0530

    Fix incorrect sorting of negative real numbers

--- src/gui/mzroll/numeric_treewidgetitem.cpp
+++ src/gui/mzroll/numeric_treewidgetitem.cpp
@@ -5,22 +5,34 @@ bool NumericTreeWidgetItem::operator<( const QTreeWidgetItem & other ) const{
     QString thisText = text(sortCol);
     QString otherText = other.text(sortCol);
 
+    // lambda to compute real value of a numeric string
+    auto textToDouble = [](QString text) {
+        QRegExp exp("^([-+])?([0-9]*\\.?[0-9]+)(?:[eE]([-+])?([0-9]+))?$");
+        if (!text.contains(exp))
+            return make_pair(false, 0.0);
+
+        double mantissaScale = exp.cap(1) == "-" ? -1.0 : 1.0;
+        double mantissa = exp.cap(2).toDouble() * mantissaScale;
+        double exponentScale = exp.cap(3) == "-" ? -1.0 : 1.0;
+        double exponent = exp.cap(4).toDouble() * exponentScale;
+        return make_pair(true, mantissa * (pow(10, exponent)));
+    };
+
+    auto convertedThisResult = textToDouble(thisText);
+    auto convertedOtherResult = textToDouble(otherText);
+    auto thisConversionSuccessful = convertedThisResult.first;
+    auto otherConversionSuccessful = convertedOtherResult.first;
+    auto thisConvertedNumber = convertedThisResult.second;
+    auto otherConvertedNumber = convertedOtherResult.second;
+    if (thisConversionSuccessful && otherConversionSuccessful) {
+        return (thisConvertedNumber < otherConvertedNumber);
+    } else if (thisConversionSuccessful) {
+        thisText = QString::fromStdString(to_string(thisConvertedNumber).c_str());
+    } else if (otherConversionSuccessful) {
+        otherText = QString::fromStdString(to_string(otherConvertedNumber).c_str());
+    }
+
     QCollator collator;
     collator.setNumericMode(true);
-
-    QRegExp exp("^[-+]?([0-9]*\\.?[0-9]+)[eE][-+]?([0-9]+)$");
-    if (thisText.contains(exp)) {
-        double mantissa = exp.cap(1).toDouble();
-        double exponent = exp.cap(2).toDouble();
-        double trueValue = mantissa * (pow(10, exponent));
-        thisText = QString::fromStdString(to_string(trueValue).c_str());
-    }
-    if (otherText.contains(exp)) {
-        double mantissa = exp.cap(1).toDouble();
-        double exponent = exp.cap(2).toDouble();
-        double trueValue = mantissa * (pow(10, exponent));
-        otherText = QString::fromStdString(to_string(trueValue).c_str());
-    }
-
     return collator.compare(thisText , otherText) < 0;
 }
