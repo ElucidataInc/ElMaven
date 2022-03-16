4216776674443ed13433de7261b06dd9a0c2b2fe
sakshikukreja14 <sakshi.kukreja@elucidata.io>
2022-03-16 20:27:03 +0530

    [fix] Include string library required in errorcodes

--- 3rdparty/ErrorHandling/errorcodes.h
+++ 3rdparty/ErrorHandling/errorcodes.h
@@ -2,28 +2,29 @@
 #define ERRORCODES_H
 
 #include <map>
+#include <string>
 
-//singleton
-//TODO: make the name of Class and filename same
+// singleton
+// TODO: make the name of Class and filename same
 class ErrorMsg
 {
     public:
-        enum Errors {
-            UnsupportedFormat,
-            FileNotFound,
-            Blank,
-            ParsemzXml,
-            ParsemzMl,
-            ParsemzData,
-            ParseCsv
-        };
+    enum Errors {
+        UnsupportedFormat,
+        FileNotFound,
+        Blank,
+        ParsemzXml,
+        ParsemzMl,
+        ParsemzData,
+        ParseCsv
+    };
 
-        static const ErrorMsg* getInstance();
-        std::string getErrmessages(const Errors& err) const;
+    static const ErrorMsg* getInstance();
+    std::string getErrmessages(const Errors& err) const;
 
     private:
-        ErrorMsg();
-        static const ErrorMsg* errMsg;
-        std::map<Errors, std::string> errMessages;
+    ErrorMsg();
+    static const ErrorMsg* errMsg;
+    std::map<Errors, std::string> errMessages;
 };
 #endif
